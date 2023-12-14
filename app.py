fc43ec7c7a5fe900c3badd9697a804273b11098e
$ⱠɆɆ₱Ɨ₦ ƉØ₲ <60668547+DeviksWeb@users.noreply.github.com>
2023-12-14 13:18:08 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, send_file, redirect, session, url_for
+import aerosandbox as asb
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+def running():
+    return render_template('index.html')
+
+@app.route("/", methods=["POST"])
+def get_values():
+    model = request.form['model']
+    quality = int(request.form['quality'])/2
+    af = asb.Airfoil(model)
+    af = af.repanel(n_points_per_side=(int(quality)))
+    now = datetime.now()
+    date_time = now.strftime("%d%m%Y%H%M%S")
+    file_name = model + "_" + str(int(quality)) + str(date_time) + ".dat"
+    af.write_dat(filepath=file_name)
+    return redirect(url_for(".download", file_name=file_name))
+
+@app.route("/download")
+def download():
+    file_name = request.args["file_name"]
+    return send_file(file_name, as_attachment=True)
